02a0ab221ce83fcfe34c6711717c1f1d96636284
Maxim Kudekro <kuderko@gmail.com>
2017-01-20 12:07:57 +0200

    fixed condition

--- src/index.php
+++ src/index.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mint
- * Date: 8/27/16
- * Time: 10:11 AM
- */
-
 require 'config.php';
 require 'Classes/Autoloader.php';
 require '../vendor/autoload.php';
@@ -14,7 +7,7 @@ if(!isset($_REQUEST['params']) && !isset($_REQUEST['source_url'])){
 	die('');
 }
 
-if(!isset($_REQUEST['source_url']) || preg_match('/^http/',$_REQUEST['source_url']) ){
+if(!preg_match('/^http/',$_REQUEST['source_url']) ){
 	die('');
 }
 $response = new Response();
